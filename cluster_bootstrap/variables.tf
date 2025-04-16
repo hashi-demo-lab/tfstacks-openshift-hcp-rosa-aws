493ad8b67ef41ae69c73f4bb02b07e595235570d
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-16 20:45:12 +1000

    Refactor variables in variables.tf: remove unused variables and update defaults for Vault Secrets Operator

--- cluster_bootstrap/variables.tf
+++ cluster_bootstrap/variables.tf
@@ -1,35 +1,15 @@
-variable "host" {
-  description = "OpenShift cluster API URL"
-  type        = string
-}
 
-variable "cluster_ca_certificate" {
-  description = "OpenShift cluster CA certificate (base64 encoded)"
-  type        = string
-}
-
-variable "client_key" {
-  description = "Client key for OpenShift authentication (base64 encoded)"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_certificate" {
-  description = "Client certificate for OpenShift authentication (base64 encoded)"
-  type        = string
-  sensitive   = true
-}
 
 variable "enable_vso" {
   description = "Enable or disable Vault Secrets Operator installation"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "create_vso_namespace" {
   description = "Create or use existing namespace for Vault Secrets Operator installation"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "vso_namespace" {
@@ -50,34 +30,36 @@ variable "enable_gitops" {
   default     = true
 }
 
-variable "create_gitops_namespace" {
-  description = "Create or use existing namespace for OpenShift GitOps Operator installation"
-  type        = bool
-  default     = true
-}
-
 variable "gitops_namespace" {
   description = "Namespace for OpenShift GitOps Operator installation"
   type        = string
   default     = "openshift-gitops-operator"
 }
 
-variable "argocd_server_addr" {
-  description = "ArgoCD server address"
-  type        = string
+variable "create_gitops_namespace" {
+  description = "Create or use existing namespace for OpenShift GitOps Operator installation"
+  type        = bool
+  default     = true
 }
 
-variable "argocd_username" {
-  description = "ArgoCD username"
-  type        = string
-  default = "admin"
-}
 
-variable "argocd_namespace" {
-  description = "Namespace for ArgoCD installation"
-  type        = string
-  default     = "openshift-gitops"
-}
+
+# variable "argocd_server_addr" {
+#   description = "ArgoCD server address"
+#   type        = string
+# }
+
+# variable "argocd_username" {
+#   description = "ArgoCD username"
+#   type        = string
+#   default = "admin"
+# }
+
+# variable "argocd_namespace" {
+#   description = "Namespace for ArgoCD installation"
+#   type        = string
+#   default     = "openshift-gitops"
+# }
 
 
 # variable "applications" {
@@ -101,22 +83,22 @@ variable "argocd_namespace" {
 # Vault variables
 
 
-variable "k8s_jwt_auth_backend" {
-  description = "Vault JWT authentication backend name used for Kubernetes"
-  type        = string
-  default     = "jwt"
-}
+# variable "k8s_jwt_auth_backend" {
+#   description = "Vault JWT authentication backend name used for Kubernetes"
+#   type        = string
+#   default     = "jwt"
+# }
 
-variable "k8s_bound_audience" {
-  type        = string
-  default = "https://kubernetes.default.svc"
-}
+# variable "k8s_bound_audience" {
+#   type        = string
+#   default = "https://kubernetes.default.svc"
+# }
 
 
-#Github variables
-variable "github_organization" {
-  description = "GitHub organization name"
-  type        = string
-  default     = "hashi-demo-lab"
-}
+# #Github variables
+# variable "github_organization" {
+#   description = "GitHub organization name"
+#   type        = string
+#   default     = "hashi-demo-lab"
+# }
 
