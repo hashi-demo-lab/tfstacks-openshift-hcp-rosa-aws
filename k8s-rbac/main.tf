b9d751126ed9a815d50ba86f3ff53c466d824caa
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-16 21:38:17 +1000

    Refactor OIDC role binding configuration and comment out unused namespace output

--- k8s-rbac/main.tf
+++ k8s-rbac/main.tf
@@ -1,26 +1,26 @@
 
 # odic-identity pre-requisite
 
-data "kubernetes_namespace" "example" {
-  metadata {
-    name = "kube-system"
-  }
-}
-
-
-# resource "kubernetes_cluster_role_binding_v1" "oidc_role" {
+# data "kubernetes_namespace" "example" {
 #   metadata {
-#     generate_name = "odic-identity"
-#   }
-
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-#   subject {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "Group"
-#     name      = var.tfc_organization_name
+#     name = "kube-system"
 #   }
 # }
+
+
+resource "kubernetes_cluster_role_binding_v1" "oidc_role" {
+  metadata {
+    generate_name = "odic-identity"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Group"
+    name      = var.tfc_organization_name
+  }
+}
--- k8s-rbac/outputs.tf
+++ k8s-rbac/outputs.tf
@@ -2,6 +2,6 @@
 #   value = kubernetes_cluster_role_binding_v1.oidc_role.id
 # }
 
-output "namespaces" {
-  value = data.kubernetes_namespace.example
-}
+# output "namespaces" {
+#   value = data.kubernetes_namespace.example
+# }
