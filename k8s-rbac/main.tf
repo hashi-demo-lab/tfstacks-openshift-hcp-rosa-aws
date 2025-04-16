fc17969fbcc29efc8f7cf1b840c6a9d71ad1dd14
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-16 21:32:23 +1000

    Refactor OIDC role binding configuration and update outputs for clarity

--- k8s-rbac/main.tf
+++ k8s-rbac/main.tf
@@ -1,18 +1,20 @@
 
 # odic-identity pre-requisite
-resource "kubernetes_cluster_role_binding_v1" "oidc_role" {
-  metadata {
-    generate_name = "odic-identity"
-  }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Group"
-    name      = var.tfc_organization_name
-  }
-}
+data "kubernetes_all_namespaces" "allns" {}
+# resource "kubernetes_cluster_role_binding_v1" "oidc_role" {
+#   metadata {
+#     generate_name = "odic-identity"
+#   }
+
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+#   subject {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "Group"
+#     name      = var.tfc_organization_name
+#   }
+# }
--- k8s-rbac/outputs.tf
+++ k8s-rbac/outputs.tf
@@ -1,3 +1,7 @@
-output "oidc_binding_id" {
-  value = kubernetes_cluster_role_binding_v1.oidc_role.id
+# output "oidc_binding_id" {
+#   value = kubernetes_cluster_role_binding_v1.oidc_role.id
+# }
+
+output "namespaces" {
+  value = data.kubernetes_all_namespaces.allns.namespaces
 }
